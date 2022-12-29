1b6e3a2beef3e47d572c6e9315fb362630606d44
kokoko12334 <gnjgnjgnj1@gmail.com>
2022-12-29 23:26:45 +0900

    20221229

--- Algorithm/트리 자료구조.py
+++ Algorithm/트리 자료구조.py
@@ -6,22 +6,65 @@
 #차수: 각 노도의 간선 개수(선분 갯수)
 #참고로 크기가 n이면 전체 간선의 갯수는 n-1이다
 
+#순회 방법
+#bfs(Breath First Search) 큐로 가능
+#계층중심으로 이루어짐
+#DFS(Depth First Search)  스택으로 가능
 
-
-n = 8
-
-for i in range(n+1):
-    print(i, '의 마지막 비트:', (i&-i))
-
-
-
-
-                        
-2&-3
-
-
-'닭고기' in '닭'
+#중위 순회(Inorder): Root를 중간에 순회(left->root->right)
+#전위 순회(Preorder): Root를 제일 먼저(root->left-> right)
+#후위 순회(Postorder): Root를 제일 나중에 순회(left->right->root)
 
 
 
-'닭' in '닭고기'
+class Treenode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Binary_search_tree():
+    def __init__(self):
+        self.root = None
+
+
+
+bst1 = Treenode(10)
+bst2 = Treenode(20)
+bst3 = Treenode(30)
+bst4 = Treenode(40)
+bst5 = Treenode(50)
+bst6 = Treenode(60)
+bst7 = Treenode(70)
+
+
+
+tree = Binary_search_tree()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
