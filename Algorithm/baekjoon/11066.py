44597b38e097d202d0f8168b2c17c6cb9d9e8420
KOMINPYO <kokoko12334>
2024-01-20 00:21:46 +0900

    adsad

--- Algorithm/baekjoon/11066.py
+++ Algorithm/baekjoon/11066.py
@@ -1,44 +1,40 @@
-
+import sys
+input = sys.stdin.readline
 t = int(input())
+
 
 
 def dp(n,arr):
     
-    dp = [[0]*n for _ in range(n)]
+    dp = [[float("inf")]*(n+1) for _ in range(n+1)]
+    sum_arr = [0] *(n+1)
+    for i in range(1,n+1):
+        dp[i][i] = 0
+        sum_arr[i] = arr[i-1] + sum_arr[i-1]
 
-    for j in range(n):
-        for i in range(n):               
-            lst = [dp[k][k+1] for k in range(i,j)]
-            if lst:
-                min_value = min(lst)
-            else:
-                min_value = 0
-            dp[i][j] = sum(arr[i:j+1]) + min_value
     
-    return dp[0][n-1]
+    for d in range(1,n):
+        
+        for i in range(1,n):
+            j = i + d
+            if j > n:
+                break
+            
+            for k in range(i,j):
+                add = (dp[i][k] + dp[k+1][j] + sum_arr[j] - sum_arr[i-1]) 
 
+                dp[i][j] = min(dp[i][j], add)
 
-def rec(i,j):
-    
-    if dp[i][j]:
-        return dp[i][j]
-
-    lst = [rec(k,k+1) for k in range(i,j)]
-    return sum(arr[i:j+1]) + min(lst)
-    
+    return dp[1][n]
+# sum_arr[j] - sum_arr[i-1] => dp[i][j] 만드는 총비용
+# (dp[i][k] + dp[k+1][j]) => dp[i][k], dp[k+1][j] 는 각 최소비용 이때 dp[1][1] 같은 것은 비용이 0이다.
 
 
 for _ in range(t):
 
     n = int(input())
     arr = [int(i) for i in input().split()]
+    a = dp(n,arr)
+    print(a)
 
-    dp = [[0]*n for _ in range(n)]
-    idx = 0
-    for i in range(n):
-        dp[idx+i][idx+i] = arr[idx+i]
-        if idx+i+1 < n:
-            dp[idx+i][idx+i+1] = sum(arr[idx+i:idx+i+2])
-
-    print(rec(0,n-1))
 
