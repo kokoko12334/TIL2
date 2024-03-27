60cb1515cba04f7b963a9475aea34967fcbf0a2c
KOMINPYO <kokoko12334>
2024-03-27 20:23:05 +0900

    add-leet2

--- Algorithm/leet/746.min-cost-climbing-stairs.py
+++ Algorithm/leet/746.min-cost-climbing-stairs.py
@@ -14,6 +14,13 @@ class Solution:
         
         return min(dp[-1],dp[-2])
     
-    
+        n = len(cost)
+        dp = [0]*(n+2)
+        # dp[1] = cost[0]
+
+        for i in range(3,n+2):
+            dp[i] = min(dp[i-1] + cost[i-2], dp[i-2] + cost[i-3])
+  
+        return dp[-1]
 a = Solution()
 a.minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])
