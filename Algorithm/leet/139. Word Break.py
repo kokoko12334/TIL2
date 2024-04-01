bd6c60984c7739fa9e5d70f94b735ea1f8da0e20
KOMINPYO <kokoko12334>
2024-04-01 13:18:50 +0900

    feat:리트dp문제

---
+++ Algorithm/leet/139. Word Break.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        
+        dp = [i==0 for i in range(n+1)]
+        
+        for i in range(1, n+1):
+            for j in range(i):
+                print(f"from:{j},to:{i}, s:{s[j:i]}")
+                if dp[j] and s[j:i] in wordDict:
+                    
+                    dp[i] = True
+                    break
+
+        return dp[-1]
