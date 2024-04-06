df69a79a21cd28642d934a6b3104441514a81060
KOMINPYO <kokoko12334>
2024-04-07 00:46:26 +0900

    feat: 리트lis문제이분탐색

---
+++ Algorithm/leet/300. Longest Increasing Subsequence.py
@@ -0,0 +1,44 @@
+from bisect import bisect_left
+from collections import defaultdict
+from typing import List
+
+#이분탐색
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        lis = []
+        lis.append(nums[0])
+        
+        n = len(nums)
+        idx_info = defaultdict(int)
+        idx_info[nums[0]] = 0
+        nn = 1
+        for i in range(1,n):
+            num = nums[i]
+            # print(f"lis:{lis}, 맨끝:{lis[-1]}, 추가되는 숫자:{num}")
+            if lis[-1] < num:
+                lis.append(num)
+                nn += 1
+                idx_info[num] = nn-1
+            else:
+                idx = bisect_left(lis,num)
+                lis[idx] = num
+                idx_info[num] = idx
+        
+
+        # 원소 찾기 => idx_info로 삽입된 숫자들의 인덱스를 기록하고 역순으로 역추적하면서 찾기
+        result = []
+        idx = n-1
+        for i in range(nn-1,-1,-1):
+            for j in range(idx,-1,-1):
+                if idx_info[nums[j]] == i:
+                    result.append(nums[j])
+                    idx -= 1
+                    break
+        
+        return nn
+
+
+
+
+
