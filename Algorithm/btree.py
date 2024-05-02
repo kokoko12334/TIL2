d77409e7176b2a8008da4bb364575334d5850916
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-05-02 23:09:36 +0900

    dasd

--- Algorithm/btree.py
+++ Algorithm/btree.py
@@ -6,7 +6,7 @@ max_int32 = 2147483647
 class Node:
 
     def __init__(self, degree):
-        self.values = deque([max_int32]* (degree - 1))
+        self.values = deque([max_int32] * (degree - 1))
         self.parent = None
         self.child = [None] * (degree+1)
         self.leaf = True
@@ -19,51 +19,66 @@ class BTree:
         self.max_key = degree - 1
         self.min_key = math.ceil(degree/2) - 1
     
-    def insert(self, num):
+    def insert(self, key):
 
         node = self.root
         #리프노드까지 내려가기
-        while node.leaf == False:
-            idx = bisect_left(node.values, num)
-            if idx:
-                node = node.child[idx - 1]
-            else:
-                node = node.child[idx]
-        
-        #노드에 데이터 삽입
-        idx = bisect_left(node.values, num)
-        node.values.insert(idx, num)
-
-        n = len(node.values)
-        if n > self.max_key:
-            median = node.values[n//2]
-            parent_node = node.parent
-            if parent_node == None:
-                self.root = Node(self.degree)
-                self.root.leaf = False
-                parent_node = self.root
+        while not node.leaf:
             
-            idx = bisect_left(parent_node.values, num)
-            parent_node.values.insert(idx, num)
+            i = bisect_left(node.values, key)
+            if node.values[i] == max_int32 or node.values[i] > key:
+                node = node.child[i]
+            else:
+                node = node.child[i + 1]
 
-            left_node = Node(self.degree)
-            right_node = Node(self.degree)
-            left_node.values = node.values[:median]
-            right_node.values = node.values[median+1:]
+        # 키 삽입
+        i = bisect_left(node.values, key)
+        node.values.insert(i, key)
+        node.values.pop()  # max_int32 제거
 
-            parent_node.child
+        # 노드 분할
+        if len(node.values) > self.max_key:
+            self.split(node)
         
+    def split(self, node):
+        mid_index = self.max_key // 2
+        mid_value = node.values[mid_index]
 
+        # 새로운 노드 생성
+        new_node = Node(self.degree)
+        new_node.values = deque([max_int32] * (self.degree - 1))
+        new_node.leaf = node.leaf
+        new_node.values.extend(node.values[mid_index + 1:])
+        new_node.child = node.child[mid_index + 1:]
+        node.values = deque(list(node.values[:mid_index]) + [max_int32] * (self.degree - 1 - mid_index))
+        node.child = node.child[:mid_index + 1]
+
+        if node.parent:
+            parent = node.parent
+        else:
+            # 새로운 루트 생성
+            parent = Node(self.degree)
+            parent.leaf = False
+            parent.child[0] = node
+            self.root = parent
+            node.parent = parent
+            new_node.parent = parent
+
+        i = bisect_left(parent.values, mid_value)
+        parent.values.insert(i, mid_value)
+        parent.values.pop()  # max_int32 제거
+        parent.child.insert(i + 1, new_node)
+
+        if len(parent.values) > self.max_key:
+            self.split(parent)
 
         #자기 자리 찾아가기 => leaf True일때까지
         #삽입 => node가 차면 다시 분기
 
-a = deque([3,5,7])
-a[2]
-a.insert(2,4)
-a
+# 사용 예
+btree = BTree(3)
+for key in [10, 20, 5, 6, 12, 30, 7, 17]:
+    btree.insert(key)
 
-bisect_left([1,2,3],5)
-len(a)
 
-a.insert()
+print(btree.root.values)
