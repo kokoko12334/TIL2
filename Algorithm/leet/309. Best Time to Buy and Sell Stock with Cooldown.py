56958201463b9bc92724b5881584ce3508a9750a
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-05-11 23:11:18 +0900

    리트코드dp

---
+++ Algorithm/leet/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sold = 0               
+        hold = float('-inf')    
+        rest = 0                
+        
+        for price in prices:    
+            prev_sold = sold    
+   
+            sold = hold + price
+            
+    
+            hold = max(hold, rest - price)
+            
+       
+            rest = max(rest, prev_sold)
+        
+
+        return max(sold, rest)
