2e100802424c44c4d94a0256b60a288d6eedb0bf
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-05-13 17:03:41 +0900

    리트코드-dp

--- Algorithm/leet/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ Algorithm/leet/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,20 +1,26 @@
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        sold = 0               
-        hold = float('-inf')    
-        rest = 0                
         
-        for price in prices:    
-            prev_sold = sold    
-   
-            sold = hold + price
-            
-    
-            hold = max(hold, rest - price)
-            
-       
-            rest = max(rest, prev_sold)
+        n = len(prices)
+        dp = [[0]*(n+1) for _ in range(4)]
         
+        dp[0][0] = -float("inf")
+        dp[3][0] = -float("inf")
+        
+        dp[0][1] = -prices[0]
+        dp[3][1] = -float("inf")
+        answer = 0
+        #0: 사고 1: 안사고, 2: 팔고, 3: 안팔고
+        # 사고 = max(이전이전 안팔고, 이전 안사고) - 해당가격
+        # 안사고 = max(이전 안사고, 이전 팔고)
+        # 팔고 = max(이전 안팔고, 이전 사고 ) + 해당가격
+        # 안팔고 = max(이전 안팔고, 이전 사고)
+        for i in range(2, n+1):
+            dp[0][i] = max(dp[2][i-2], dp[1][i-1]) - prices[i-1]
+            dp[1][i] = max(dp[1][i-1], dp[2][i-1])
+            dp[2][i] = max(dp[0][i-1], dp[3][i-1]) + prices[i-1]
+            dp[3][i] = max(dp[0][i-1], dp[3][i-1])
+            answer = max(answer, dp[0][i], dp[1][i], dp[2][i], dp[3][i])
 
-        return max(sold, rest)
+        return answer
