46dbd032110e47710b2d8c0a7f41614b9d800135
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-07-16 23:31:29 +0900

    leet

---
+++ Algorithm/leet/38. Count and Say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+
+        if n == 1:
+            return '1'
+        
+        string = self.countAndSay(n-1)
+        arr = []
+        s = ""
+        cnt = 1
+        for i in range(len(string)):
+            if string[i] == s:
+                cnt += 1
+            else:
+                arr.append(str(cnt))
+                arr.append(s)
+                s = string[i]
+                cnt = 1
+        arr.append(str(cnt))
+        arr.append(s)
+        result = "".join(arr[2:])
+        return result
+        
