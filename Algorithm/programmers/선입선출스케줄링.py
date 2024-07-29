396780d4d4c6247132f6ebc05deaa0c50ef7c089
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-07-29 15:49:45 +0900

    programmers

--- Algorithm/programmers/선입선출스케줄링.py
+++ Algorithm/programmers/선입선출스케줄링.py
@@ -1,30 +1,35 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/12920
-from bisect import bisect_right
 def solution(n, cores):
     
     core_n = len(cores)
     if core_n >= n:
         return n
-    arr = [(cores[i], i+1) for i in range(core_n)]
-    arr.sort(key = lambda x: x[0])
-    tasks = n - core_n
-    h = 1
+
+    tasks = n - core_n  
+    l = 0  
+    r = max(cores) * n 
+    # print(f"남은 작업량:{tasks}")
+    while l < r:
+        mid = (l + r) // 2
+        result = sum(mid // core for core in cores)
+        # print(f"{mid}시간:{result}작업량, l,r{l, r}")
+        if result >= tasks:
+            r = mid
+        else:
+            l = mid + 1 
+            
+    h = l
+    # print(f"h:{h}")
+    pre_h = h - 1
+    result = sum(pre_h // core for core in cores)
+    tasks -= result  
+
     answer = 0
-    arr2 = [i[0] for i in arr]
-    print(f"arr:{arr}")
-    while tasks > 0:
-        cnt = bisect_right(arr2, h)
-        print(f"남은코어수:{cnt}, h:{h}")
-        tasks -= cnt
-        print(f"남은 태스크:{tasks}")
-        if tasks <= 0:
-            answer_idx = cnt + tasks - 1
-            answer_arr = sorted(arr[:cnt], key=lambda x: x[1])
-            print(f"마지막에서 가장 앞선 코어:{answer_arr}")
-            print(f"마지막코어:{answer_idx}")
-            answer = answer_arr[answer_idx][1]
+    for i in range(core_n):
+        if h % cores[i] == 0:  
+            tasks -= 1
+        
+        if tasks == 0:  
+            answer = i + 1 
             break
-        
-        h += 1
-        
+            
     return answer
