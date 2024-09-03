a4266adc884238ab7471cd0e8959eef6772049f0
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-09-03 18:36:24 +0900

    lang

--- lang/test.py
+++ lang/test.py
@@ -1,64 +1,44 @@
-"""
-
-검색단계: 사용자의 의도를 파악해서 이를 토대로 원하는 문서를 검색
-생성단계: 검색된 데이터를 기반으로 LLM모델에 넣어서 답변을 생성
-
-"""
-
 from dotenv import load_dotenv
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 load_dotenv()
-
-
-#1. 파일 추출
-file_path = "./[위메프]연결감사보고서(2024.04.09).pdf"
-loader = PyPDFLoader(file_path)
-file = loader.load()
-
-
-#2. 파일 청크 생성
+import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-splites = text_splitter.split_documents(file)
-
-print(len(splites)) # 총 청크된 문서 크기
-print(splites[1]) # 하나의 청크
-print(splites[1].metadata) # 메타데이터
-print(splites[3].page_content)# 내용 
-
-#3. 인덱싱
-
 from langchain.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from tqdm import tqdm
+# 지정된 디렉토리 경로
+
+directory = r"C:/Users/KMP/Desktop/dart/company_pre_report"
+files = []
+for filename in os.listdir(directory):
+    files.append(directory+"/"+filename)
 
 embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
-vectorstore = Chroma.from_documents(
-                    persist_directory="D:/chroma",
-                    collection_name="dart_test",
-                    documents=splites,
-                    embedding=embedding_model)
+
+for file_path in tqdm(files[:10]):
+    #1. 파일 추출
+    loader = TextLoader(file_path)
+    file = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    splites = text_splitter.split_documents(file)
+
+    from langchain.vectorstores import Chroma
+    from langchain_openai import OpenAIEmbeddings
+
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    db = Chroma.from_documents(persist_directory="D:/chroma",
+                        collection_name="dart_test2",
+                        embedding=embedding_model,
+                        documents=splites,
+    )
 
 
-docs = vectorstore.similarity_search(
-    query="요즘 경영 위기, 위험한 기업들", 
+
+docs = db.similarity_search(
+    query="요즘뜨는 기업", 
     k=5)
-print(len(docs)) # 결과 내역
-
-docs[0].metadata
-for doc in docs:
-    print(doc.page_content)
-
-import chromadb
-
-c = chromadb.PersistentClient("D:/chroma")
-
-coll = c.get_collection("dart_test")
 
 docs
-coll.get(ids=['0063e947-5b2c-478a-8a24-20fd6f3095a1'], include=['embeddings','metadatas','documents'])
-coll.get()
-
 #4 검색(Retieval)
 
 from langchain_openai import ChatOpenAI
@@ -100,4 +80,8 @@ rag_chain = (
 
 res = rag_chain.invoke("요즘 뜨는 기업")
 print(res)
+
+
+
+
 
