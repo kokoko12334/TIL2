6c30bd385b10778d6c9feeb30927508ab7574451
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-09-06 11:58:11 +0900

    baek

--- Algorithm/baekjoon/3190.py
+++ Algorithm/baekjoon/3190.py
@@ -31,31 +31,33 @@ change_d = {
 
 q = deque([(0,0,'r')])
 go = [0,1]
-body = deque()
+body = deque([(0,0)])
+m[0][0] = -1
 s = 0
 answer = 0
 
 while q:
     y, x, d = q.popleft()
-    m[y][x] = -1
-    body.append((y,x))
-    if s in move.keys():
-        keyy = d + move[s]
-        go, d = change_d[keyy]
-
+    s += 1
     ny = y + go[0]
     nx = x + go[1]
 
     if nx < 0 or nx >= n or ny < 0 or ny >= n or m[ny][nx] == -1:
-        answer = s + 1
+        answer = s
         break
     
-    if (ny, nx) != (0, 0) and m[ny][nx] != 1:
+    if m[ny][nx] != 1:
         t_y, t_x = body.popleft()
         m[t_y][t_x] = 0
     else:
         m[ny][nx] = 0
+    m[ny][nx] = -1
+    if s in move.keys():
+        keyy = d + move[s]
+        go, d = change_d[keyy]
+    
+    body.append([ny, nx])
     q.append((ny, nx, d))
-    s += 1
+
 
 print(answer)
