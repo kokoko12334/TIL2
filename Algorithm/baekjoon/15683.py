3840d2501e816bb73f1267fa855c43cb77ca1564
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-09-12 18:36:26 +0900

    baek

--- Algorithm/baekjoon/15683.py
+++ Algorithm/baekjoon/15683.py
@@ -1,61 +1,68 @@
+import sys
+input = sys.stdin.readline
+n, m = [int(i) for i in input().split()]
+
+matrix = []
+for _ in range(n):
+    matrix.append([int(i) for i in input().split()])
 
 
-
-def search(lst,i,j):
-    v = lst[i][j]
-    y,x = i,j
-    di = [0,0,0,0]
-    dx = [1,0,-1,0]
-    dy = [0,1,0,-1]
-    idx = -1
-    for i in range(4):
-        idx += 1
-        ny = dy[i] + y
-        nx = dx[i] + x
-        if nx < 0 or nx>= m or ny <0 or ny >= n:
-            continue
-        while (0<=ny and ny<n) and (0<=nx and nx<m) :
-            if lst[ny][nx] == 6:
-                break
-            if lst[ny][nx] == 0:
-                di[idx] += 1
-            if ny > y:
-                ny += 1
-            elif ny < y:
-                ny -= 1
-            elif nx > x:
-                nx += 1
-            elif nx < x:
-                nx -= 1
-    return di
-
-
-n,m = [int(i) for i in input().split()]
-
-lst = [[int(i) for i in input().split()]for _ in range(n)]
-
-
-
-for i in lst:
-    print(i)
-
-total = n*m
-stack = []
+rotate = {
+    (0,1):(1,0),
+    (1,0):(0,-1),
+    (0,-1):(-1,0),
+    (-1, 0):(0,1),
+}
+g = {
+    1: [(0,1)],
+    2: [(0,1), (0,-1)],
+    3: [(-1, 0), (0,1)],
+    4: [(0,-1), (-1,0), (0,1)],
+    5: [(1, 0), (0,-1), (-1,0), (0,1),],
+}
+arr = []
 for i in range(n):
     for j in range(m):
-        if lst[i][j]:
-            total -= 1
-            if lst != 6:
-                stack.append([i,j])
+        if matrix[i][j] in {1,2,3,4,5}:
+            arr.append((i,j,matrix[i][j]))
 
 
-maxx = float("int")
+def dfs(i, arr, matrix):
 
-##최소값
+    y, x, num = arr[i]
+
+    go = g[num]
+    case = [go]
+    if num in {1, 3, 4}:
+        for _ in range(3):
+            tmp = []
+            for i in go:
+                tmp.append(rotate[i])
+            case.append(tmp)
+
+    elif num == 2:
+        for _ in range(1):
+            tmp = []
+            for i in go:
+                tmp.append(rotate[i])
+            case.append(tmp)
+
+    
+    for move in case:
+        for i in range(len(move)):
+            ny = max(move[i][0] * (n-y) + y, 0)
+            nx = max(move[i][1] * (m-x) + x, 0)
+        
+            print(ny,nx)
+
+    return
+        
+
+dfs(2, arr, matrix)
 
 
-
-
-
-
-
+# y,x = (2, 2)
+# for i in range(len(move5)):
+#     ny = max(move5[i][0] * (n-y) + y, 0)
+#     nx = max(move5[i][1] * (m-x) + x, 0)
+#     print(ny,nx)
