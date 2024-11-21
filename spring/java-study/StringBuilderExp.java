e1f1bfc47b3dc989d66e519b497c0899836ea849
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-21 23:57:34 +0900

    java

---
+++ spring/java-study/StringBuilderExp.java
@@ -0,0 +1,27 @@
+public class StringBuilderExp {
+
+    public static void main(String[] args) {
+        // 테스트 반복 횟수
+        int iterations = 100000;
+
+        // String을 사용한 문자열 결합
+        long startTime = System.nanoTime();
+        String str = "";
+        for (int i = 0; i < iterations; i++) {
+            str = str + "a";  // 새로운 String 객체가 매번 생성됨
+        }
+        long endTime = System.nanoTime();
+        double stringTime = (endTime - startTime) / 1_000_000_000.0;
+        System.out.println("String을 사용한 결합 시간: " + stringTime + "s");
+
+        // StringBuilder를 사용한 문자열 결합
+        startTime = System.nanoTime();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < iterations; i++) {
+            sb.append("a");  // 내부 버퍼를 수정
+        }
+        endTime = System.nanoTime();
+        double stringBuilderTime = (endTime - startTime) / 1_000_000_000.0;
+        System.out.println("StringBuilder를 사용한 결합 시간: " + stringBuilderTime + "s");
+    }
+}
---
+++ spring/java-study/WrapperClassTest.java
@@ -0,0 +1,33 @@
+public class WrapperClassTest {
+    private static int counter = 0;
+
+    public static void main(String[] args) {
+        int iter = 10000;
+        Runnable task = () -> {
+            for (int i = 0; i < iter; i++) {
+                counter++;  // 동시성 문제 발생 가능
+            }
+        };
+
+        Runnable task2 = () -> {
+            for (int i = 0; i < iter; i++) {
+                counter--;  // 동시성 문제 발생 가능
+            }
+        };
+
+        // 2개의 스레드 생성
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task2);
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("counter = " + counter);  // 예상과 다를 수 있음
+    }
+}
