bf0fb5693e4c6acb782397be5ca0d121807172dc
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-26 20:08:49 +0900

    jpa

--- spring/basic-jpa/src/main/java/hellojpa/JpaMain.java
+++ spring/basic-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,17 +16,43 @@ public class JpaMain {
 
         try {
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-
-            for (Member member: result) {
-                System.out.println("memberName = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//
+//            for (Member member: result) {
+//                System.out.println("memberName = " + member.getName());
+//            }
 
 //            Member member = em.find(Member.class, 3L);
 //            member.setName("Hel1"); // 변경등 수정사항은 트랜잭션이 알아서 persist함
 //            em.persist(member); // 저장
+
+//            long start = System.currentTimeMillis();
+//            for (long i=1L; i <= 10000L; i++) {
+////                tx.begin();
+//                Member member = new Member();
+//                member.setId(i);
+//                member.setName("name" + i);
+//                em.persist(member);
+////                tx.commit();
+//            }
+
+            Member member1 = em.find(Member.class, 200L);
+            System.out.println(member1.getName());
+            System.out.println("#######################");
+
+            Member member2 = em.find(Member.class, 200L);
+            System.out.println(member2.getName());
+            System.out.println("#######################");
+
+            em.detach(member1);
+            Member member3 = em.find(Member.class, 200L);
+            System.out.println(member3.getName());
+            System.out.println("#######################");
             tx.commit();
+//            long end = System.currentTimeMillis();
+
+//            System.out.println("걸린시간(초): " + (end - start));
         } catch (Exception e) {
             tx.rollback();
         } finally {
