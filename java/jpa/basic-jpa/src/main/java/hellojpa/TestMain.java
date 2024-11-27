2f03129997758b8a12e0951fb5d0a7050b909ec3
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-27 22:12:33 +0900

    jpa

---
+++ java/jpa/basic-jpa/src/main/java/hellojpa/TestMain.java
@@ -0,0 +1,29 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TestMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        try{
+            tx.begin();
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("kd");
+            em.persist(member);
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
