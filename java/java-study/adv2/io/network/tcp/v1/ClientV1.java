87ced49bf50b7fc85868b323541361ca40951edc
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-12-01 19:37:20 +0900

    ja

---
+++ java/java-study/adv2/io/network/tcp/v1/ClientV1.java
@@ -0,0 +1,36 @@
+package adv2.io.network.tcp.v1;
+
+import adv2.io.util.MyLogger;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static adv2.io.util.MyLogger.log;
+
+public class ClientV1 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        Socket socket = new Socket("localhost", PORT);
+
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        log("소켓 연결: " + socket);
+
+        String toSend =  "hello";
+        output.writeUTF(toSend);
+        log("client -> server: " + toSend);
+
+        String received = input.readUTF();
+        log("client <- server: " + received);
+
+        log("연결 종료:" + socket);
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
---
+++ java/java-study/adv2/io/network/tcp/v1/InetAddressMain.java
@@ -0,0 +1,14 @@
+package adv2.io.network.tcp.v1;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressMain {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress localhost = InetAddress.getByName("localhost");
+        System.out.println(localhost);
+
+        InetAddress google = InetAddress.getByName("naver.com");
+        System.out.println(google);
+    }
+}
