622007866c42f3fecf62025baf549fcb41486941
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-01-22 10:20:54 +0900

    pu

--- Algorithm/python/baekjoon/10775.py
+++ Algorithm/python/baekjoon/10775.py
@@ -2,7 +2,34 @@ import sys
 
 input = sys.stdin.readline
 
+def find_parent(x):
+    if x != parent[x]:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+
+def union_parent(x, y):
+    x = find_parent(x)
+    y = find_parent(y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
 g = int(input())
 p = int(input())
+parent = [i for i in range(g+1)]
+plane_arr = []
+for _ in range(p):
+    plane_arr.append(int(input()))
 
-plane_arr = [int(i) for i in input().split()]
+
+count = 0
+for p in plane_arr:
+    x = find_parent(p)
+    if x == 0:
+        break
+    union_parent(x, x-1)
+    count += 1
+
+print(count)
