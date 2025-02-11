a5952294ea0ddb9c4d0e46408e06d7abb40065b2
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-02-11 23:10:12 +0900

    pycoding

--- Algorithm/python/baekjoon/4386.py
+++ Algorithm/python/baekjoon/4386.py
@@ -5,6 +5,45 @@ for _ in range(n):
     y, x = [float(i) for i in input().split()]
     starts.append([y, x])
 
+
+g = [[0] * n for _ in range(n)]
+
+
+def initialize(n):
+    parent = [i for i in range(n)]
+    rank = [0] * n
+    return parent, rank
+ 
+def find(x, parent):
+    if x != parent[x]:
+        parent[x] = find(parent[x], parent)
+    return parent[x]
+    
+def union(x, y, parent, rank):
+    xroot = find(x, parent)
+    yroot = find(y, parent)
+ 
+    if xroot == yroot:
+        return
+ 
+    if rank[xroot] < rank[yroot]:
+        parent[xroot] = yroot
+    elif rank[xroot] > rank[yroot]:
+        parent[yroot] = xroot
+    else:
+        parent[yroot] = xroot
+        rank[xroot] += 1
+
+
+
+
+
+
+
+
+
+
+
 def search(idx):
     total = 0
     y, x = starts[idx][0], starts[idx][1]
