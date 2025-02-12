459967e1e0ec73934b5ca8349b37a60933708647
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-02-12 09:37:13 +0900

    py

--- Algorithm/python/baekjoon/4386.py
+++ Algorithm/python/baekjoon/4386.py
@@ -5,9 +5,7 @@ for _ in range(n):
     y, x = [float(i) for i in input().split()]
     starts.append([y, x])
 
-
 g = [[0] * n for _ in range(n)]
-
 
 def initialize(n):
     parent = [i for i in range(n)]
@@ -34,47 +32,3 @@ def union(x, y, parent, rank):
         parent[yroot] = xroot
         rank[xroot] += 1
 
-
-
-
-
-
-
-
-
-
-
-def search(idx):
-    total = 0
-    y, x = starts[idx][0], starts[idx][1]
-    seen = {i for i in range(n)}
-    seen.remove(idx)
-
-    stack = [(y, x)]
-    while stack:
-        y, x = stack.pop()
-
-        current = [float("inf"), -1]
-        for i in seen:
-            ny, nx = starts[i]
-
-            dis = ((ny - y)**2 + (nx - x)**2)**(1/2)
-
-            if current[0] > dis:
-                current = [dis, i]
-        
-        next_idx = current[1]
-        total += current[0]
-
-        seen.remove(next_idx)
-        stack.append((starts[next_idx][0], starts[next_idx][1]))
-        if not seen:
-            break
-
-    return total
-
-answer = float("inf")
-for i in range(n):
-    answer = min(answer, search(i))
-
-print(format(answer, ".2f"))
